6717a9a7069af2d4c03a4b56f63b555c415f4155
Vladimir Buchatskiy <vladimirbuchatskiy@gmail.com>
2020-10-27 22:09:13 +0200

    scroll done

---
+++ src/components/Search/Search.vue
@@ -0,0 +1,34 @@
+<template>
+  <input
+    class="search"
+    @input="ontype($event.target.value)"
+    placeholder="Search"
+  />
+</template>
+
+<script>
+import { debounce } from "lodash";
+
+export default {
+  name: "Search",
+  data() {
+    return {
+      query: ""
+    };
+  },
+  methods: {
+    ontype: debounce(function(value) {
+      this.query += value;
+      if ([...this.query].length > 3) {
+        this.$emit("searching", value);
+      }
+    }, 100)
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.search {
+  height: 2em;
+}
+</style>
